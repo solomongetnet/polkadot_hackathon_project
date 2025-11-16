4e3e0c2d41f5555035fa76f2620ea0e5a3e1f482
Solomon Getnet <solomongetnet.dev@gmail.com>
2025-11-16 21:07:02 +0300

    new change

--- client/src/app/admin/users/filter-container.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-const FilterContainer = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-
-  return <div></div>;
-};
-
-export default FilterContainer;
