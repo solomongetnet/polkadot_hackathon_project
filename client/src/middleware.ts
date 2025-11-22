35ac6a73be529310d93cc3aca20f8a0cb7a43f69
Solomon Getnet <solomongetnet.dev@gmail.com>
2025-11-22 16:23:35 +0300

    new

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -27,5 +27,5 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"], // Apply middleware to all routes except API and static files
-  // runtime: "nodejs",
+  runtime: "nodejs",
 };
